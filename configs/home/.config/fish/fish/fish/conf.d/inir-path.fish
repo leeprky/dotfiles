23376892d9cefbb0b60359a15a687f6951df3142
Lee Parky <lspky@protonmail.com>
2026-06-25 14:34:53 +0100

    Update config snapshot 2026-06-25

---
+++ configs/home/.config/fish/fish/fish/conf.d/inir-path.fish
@@ -0,0 +1,3 @@
+if not contains -- "/home/leeparky04/.local/bin" $PATH
+    set -gx PATH "/home/leeparky04/.local/bin" $PATH
+end
---
+++ configs/home/.config/spicetify/spicetify/spicetify/CustomApps/marketplace/$HOME/.local/share/spotify-launcher/install/usr/share/spotify/Apps/xpui/dev-playground.js
@@ -0,0 +1,2 @@
+"use strict";(("undefined"!=typeof self?self:global).webpackChunkclient_web=("undefined"!=typeof self?self:global).webpackChunkclient_web||[]).push([["1971"],{5072(e,l,f){f.r(l),f.d(l,{default:()=>s});var n=f(86070);let s=()=>(0,n.jsx)("div",{})}}]);
+//# sourceMappingURL=dev-playground.js.map
